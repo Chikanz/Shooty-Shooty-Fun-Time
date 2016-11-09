5b5373e88794d80d5585ca857582c6ad92ab694a
dook - home PC <chikanz@outlook.com>
2016-11-09 23:23:24 +1100

    made shooty ball arena visible, smaller. messed around with black friday items

---
+++ Assets/Scripts/gravItem.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gravItem : MonoBehaviour {
+
+
+    Rigidbody RB;
+    BoxCollider BC;
+    // Use this for initialization
+    void Start ()
+    {
+        BC = GetComponent<BoxCollider>();        
+        RB = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+
+    public void OnTriggerStay(Collider c)
+    {
+        if(c.tag == "Player")
+        {
+            Vector3 pPos = c.transform.position;
+
+            // Get a direction to the player
+            //(from https://docs.unity3d.com/Manual/DirectionDistanceFromOneObjectToAnother.html)
+            var heading = pPos - transform.position;
+            var distance = heading.magnitude;
+            var direction = heading / distance; // This is now the normalized direction.
+
+            RB.AddForce(direction * (GetComponent<SphereCollider>().radius - distance) * 1.5f);
+        }
+    }
+
+    public void OnCollisionEnter(Collision c)
+    {
+        if (c.gameObject.tag == "Player")
+            Destroy(gameObject);
+    }
+}
